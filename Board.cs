bba9c244bc20ab032a1769bd8a2eec7ffc0b6744
DESKTOP-3RA1E18\ip323 <ip323@naver.com>
2023-03-09 19:40:28 +0900

    Commit Test

--- Board.cs
+++ Board.cs
@@ -8,8 +8,13 @@ namespace Algorithm
 {
     internal class Board
     {
+        // 배열 : 크기 변경 불가
         public int[] _data = new int[25];
+
+        // 동적배열 : 크기 변경 가능(중간 삽입/삭제 불가)
         public List<int> _data2 = new List<int>();
+
+        // 연결리스트 : 크기 변경 및 중간 삽입/삭제 가능
         public LinkedList<int> _data3 = new LinkedList<int>();
 
         public void Initialize()
